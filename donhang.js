50483061266402d94208facc8c8626e99b564a45
Tarykege <xoiiwkingg@gmail.com>
2021-11-02 10:13:23 +0700

    adding donhang

---
+++ donhang.js
@@ -0,0 +1,8 @@
+var soLuong = document.getElementsByClassName('soluong')
+var gia = document.getElementsByClassName('gia')
+var thanhTien = document.getElementsByClassName('thanhtien')
+var tongThanhTien = document.getElementsByClassName('tongthanhtien')
+var chietKhau = document.getElementsByClassName('chietkhau')
+var thue = document.getElementsByClassName('thue')
+var tongDonHang = document.getElementsByClassName('tongdonhang')
+
